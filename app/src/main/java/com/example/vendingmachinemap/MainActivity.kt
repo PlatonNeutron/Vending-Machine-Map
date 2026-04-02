247fc0ed7aa4ea6975970dc0d0af31115f259166
Platon Neutron <mr.celioofficiel@gmail.com>
2026-04-02 12:28:37 -0400

    Moving the navBar in it's own file

--- app/src/main/java/com/example/vendingmachinemap/MainActivity.kt
+++ app/src/main/java/com/example/vendingmachinemap/MainActivity.kt
@@ -2,33 +2,21 @@ package com.example.vendingmachinemap
 
 // Imports
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.vendingmachinemap.navigation.AppNavHost
+
 import com.example.vendingmachinemap.ui.theme.VendingMachineMapTheme
-import com.example.vendingmachinemap.navigation.Destinations
+import com.example.vendingmachinemap.ui.components.NavigationBar
+
 
 // MainActivity
 class MainActivity : ComponentActivity() {
@@ -58,40 +46,6 @@ fun AccountScreen() {
         Text("Account Screen")
     }
 }
-
-// Logique de la navigation
-// Déclaration des components
-@Composable
-fun NavigationBar() {
-    val navController = rememberNavController()
-    val startDestination = Destinations.Map
-    var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
-
-    Scaffold(
-        //modifier = modifier,
-        bottomBar = {
-            NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
-                Destinations.entries.forEachIndexed { index, destinations ->
-                    NavigationBarItem(
-                        selected = selectedDestination == index,
-                        onClick = {
-                            navController.navigate(route = destinations.route)
-                            selectedDestination = index
-                        },
-                        icon = {
-                            Icon(
-                                destinations.icon,
-                                contentDescription = destinations.label
-                            )
-                        },
-                        label = { Text(destinations.label) }
-                    )
-                }
-            }
-        }
-    ) { contentPadding -> AppNavHost(navController, startDestination, modifier = Modifier.padding(contentPadding)) }
-}
-
 
 
 
---
+++ app/src/main/java/com/example/vendingmachinemap/ui/components/NavigationBar.kt
@@ -0,0 +1,50 @@
+package com.example.vendingmachinemap.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.vendingmachinemap.navigation.AppNavHost
+import com.example.vendingmachinemap.navigation.Destinations
+
+// Creation of the NavBar
+@Composable
+fun NavigationBar() {
+    val navController = rememberNavController()
+    val startDestination = Destinations.Map
+    var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+
+    Scaffold(
+        //modifier = modifier,
+        bottomBar = {
+            NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+                Destinations.entries.forEachIndexed { index, destinations ->
+                    NavigationBarItem(
+                        selected = selectedDestination == index,
+                        onClick = {
+                            navController.navigate(route = destinations.route)
+                            selectedDestination = index
+                        },
+                        icon = {
+                            Icon(
+                                destinations.icon,
+                                contentDescription = destinations.label
+                            )
+                        },
+                        label = { Text(destinations.label) }
+                    )
+                }
+            }
+        }
+    ) { contentPadding -> AppNavHost(navController, startDestination, modifier = Modifier.padding(contentPadding)) }
+}
