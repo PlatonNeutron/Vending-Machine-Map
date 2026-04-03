1e7baf09d43990c37743ebf9d9719b7bdabcc80e
Platon Neutron <mr.celioofficiel@gmail.com>
2026-04-03 14:24:45 -0400

    Account screen display login or loged screens depending on state

--- app/src/main/java/com/example/vendingmachinemap/ui/screens/accountScreens/AccountScreen.kt
+++ app/src/main/java/com/example/vendingmachinemap/ui/screens/accountScreens/AccountScreen.kt
@@ -1,5 +1,6 @@
 package com.example.vendingmachinemap.ui.screens.accountScreens
 
+// Imports
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
@@ -7,24 +8,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.vendingmachinemap.ui.screens.AppStates
+
+import com.example.vendingmachinemap.ui.screens.accountScreens.loginScreen.LoginContent
 import com.example.vendingmachinemap.ui.theme.VendingMachineMapTheme
 
-const val isConnected = true
-
 @Composable
-fun AccountScreen() {
-    if (isConnected) {
-        AccountContent()
-    }
-    else {
-        ConnexionContent()
-    }
-}
+fun AccountScreen(viewModel: AccountViewModel = viewModel()) {
+    val test = viewModel.getState()
+    println(test)
 
-@Composable
-fun ConnexionContent() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Non connecté")
+    when (test) {
+        AppStates.UiStates.Account.SignedOut -> LoginContent()
+        /*AppStates.UiStates.Account.SignedIn -> AccountContent()*/
+        else -> AccountContent()
     }
 }
 
