1753886a21f36f7b7073e9255d2c0f113114580d
Platon Neutron <mr.celioofficiel@gmail.com>
2026-04-07 18:52:46 -0400

    Forgot the index

--- app/src/main/java/com/example/vendingmachinemap/model/domain/VendingMachinesEntity.kt
+++ app/src/main/java/com/example/vendingmachinemap/model/domain/VendingMachinesEntity.kt
@@ -2,13 +2,15 @@ package com.example.vendingmachinemap.model.domain
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "vendingMachines",
     foreignKeys = [
         ForeignKey(entity = UsersEntity::class,
             parentColumns = ["id"],
-            childColumns = ["addedBy"])])
+            childColumns = ["addedBy"])],
+    indices = [Index("addedBy")])
 data class VendingMachinesEntity(
     @PrimaryKey(autoGenerate = true) val id : Int = 0,
     val name : String?,
