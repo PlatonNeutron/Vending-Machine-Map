5d68e8bd5d6046b9edf46429f0d3bf60eb1750df
Platon Neutron <mr.celioofficiel@gmail.com>
2026-04-30 01:26:25 -0400

    Update UsersDAO.kt

--- app/src/main/java/com/example/vendingmachinemap/model/data/dao/UsersDAO.kt
+++ app/src/main/java/com/example/vendingmachinemap/model/data/dao/UsersDAO.kt
@@ -1,12 +1,21 @@
 package com.example.vendingmachinemap.model.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Query
+import androidx.room.Upsert
 import com.example.vendingmachinemap.model.domain.UsersEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UsersDAO {
-    @Query("SELECT mail, mdpHash FROM users")
+
+    @Upsert
+    suspend fun upsertUser(usersEntity: UsersEntity)
+
+    @Delete
+    suspend fun deleteUser(usersEntity: UsersEntity)
+
+    @Query("SELECT * FROM users WHERE userName = mail")
     fun getCredentials(mail: String) : Flow<List<UsersEntity>>
 }
