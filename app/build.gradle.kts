87d3db649c7d4e593e616db69dc6fa503cbfdebb
Platon Neutron <mr.celioofficiel@gmail.com>
2026-04-30 15:26:22 -0400

    Change in lib version to patch an error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,12 +16,12 @@ val mapsApiKey: String = localProperties.getProperty("MAPS_API_KEY") ?: ""
 
 android {
     namespace = "com.example.vendingmachinemap"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.vendingmachinemap"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -61,7 +61,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -71,15 +70,8 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.maps.compose)
     implementation(libs.play.services.maps)
-    implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
-}
-
-configurations.all {
-    resolutionStrategy {
-        force("androidx.core:core:1.12.0")
-        force("androidx.core:core-ktx:1.12.0")
-    }
 }
